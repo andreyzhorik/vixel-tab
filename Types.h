1f4b46fae9e8dd41f8b33e28da056290b712f09c
andrey <abdalovandrey95@gmail.com>
2026-01-18 19:55:13 -0800

    Refactor Gesture enum and remove unused defines

--- Types.h
+++ Types.h
@@ -1,15 +1,9 @@
 #pragma once
-#include <Arduino.h>
-
-#define SCREEN_WIDTH  240
-#define SCREEN_HEIGHT 320
-
-#define SWIPE_THRESHOLD 50
-#define BOTTOM_ZONE 40
 
 enum Gesture {
-  GESTURE_NONE,
+  NONE,
+  TAP,
   SWIPE_LEFT,
   SWIPE_RIGHT,
-  SWIPE_UP_HOME
+  SWIPE_UP
 };
